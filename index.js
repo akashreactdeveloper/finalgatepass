b4d3c056f900944ae9ce4a21e51f60619c16785a
Your Name <your-email@example.com>
2024-08-14 12:41:31 +0530

    changes

--- index.js
+++ index.js
@@ -7,8 +7,17 @@ const cookieParser = require('cookie-parser')
 const AwlModel = require('./models/AwlModel');
 
 const app = express()
+const allowedOrigins = ['https://66bc56186652184c2bb2efba--teal-yeot-b589c2.netlify.app/'];
 app.use(cors({
-    origin: '*',
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     credentials: true
 }));
 
