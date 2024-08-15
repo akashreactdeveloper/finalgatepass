78b09a1ac4a4b4d3db5cf4dda6c69ad0a4cecf5b
Your Name <your-email@example.com>
2024-08-15 22:34:31 +0530

    main

--- controller/user/userSignin.js
+++ controller/user/userSignin.js
@@ -2,59 +2,63 @@ const bcrypt = require('bcryptjs')
 const userModel = require('../../models/userModel')
 const jwt = require('jsonwebtoken');
 
-async function userSignInController(req, res) {
-  try {
-    const { email, password } = req.body
+async function userSignInController(req,res){
+    try{
+        const { email , password} = req.body
 
-    if (!email) {
-      throw new Error("Please provide email")
-    }
-    if (!password) {
-      throw new Error("Please provide password")
-    }
+        if(!email){
+            throw new Error("Please provide email")
+        }
+        if(!password){
+             throw new Error("Please provide password")
+        }
 
-    const user = await userModel.findOne({ email })
+        const user = await userModel.findOne({email})
 
-    if (!user) {
-      throw new Error("User not found")
-    }
+       if(!user){
+            throw new Error("User not found")
+       }
 
-    const checkPassword = await bcrypt.compare(password, user.password)
+       const checkPassword = await bcrypt.compare(password,user.password)
 
-    console.log("checkPassoword", checkPassword)
+       console.log("checkPassoword",checkPassword)
 
-    if (checkPassword) {
+       if(checkPassword){
         const tokenData = {
-            _id: user._id,
-            email: req.body.email, // Use req.body.email instead of user.email
-          }
-          console.log("User object:", user);
-      const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, { expiresIn: 60 * 60 * 8 });
+            _id : user._id,
+            email : user.email,
+        }
+        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, { expiresIn: 60 * 60 * 8 });
 
-      const tokenOption = {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'strict'
-      }
+        const tokenOption = {
+            httpOnly : true,
+            secure : true
+        }
 
-      res.cookie("token", token, tokenOption).status(200).json({
-        message: "Login successfully",
-        data: token,
-        success: true,
-        error: false
-      })
+        res.cookie("token",token,tokenOption).status(200).json({
+            message : "Login successfully",
+            data : token,
+            success : true,
+            error : false
+        })
 
-    } else {
-      throw new Error("Please check Password")
+       }else{
+         throw new Error("Please check Password")
+       }
+
+
+
+
+
+
+
+    }catch(err){
+        res.json({
+            message : err.message || err  ,
+            error : true,
+            success : false,
+        })
     }
-
-  } catch (err) {
-    res.json({
-      message: err.message || err,
-      error: true,
-      success: false,
-    })
-  }
 
 }
 
