d1cb90c3c77ec789965b9c0ebf687cbd5dda3229
Your Name <your-email@example.com>
2024-08-15 22:23:50 +0530

    changes

--- middleware/authToken.js
+++ middleware/authToken.js
@@ -2,36 +2,39 @@ const jwt = require('jsonwebtoken');
 
 async function authToken(req, res, next) {
     try {
-      const token = req.cookies?.token;
-  
-      if (!token) {
+        const token = req.cookies?.token;
+
+        if (!token) {
+            return res.status(401).json({
+                message: "Please Login...!",
+                error: true,
+                success: false,
+            });
+        }
+
+        const decoded = await jwt.verify(token, process.env.TOKEN_SECRET_KEY);
+        console.log("decoded", decoded)
+        req.authUserId = decoded._id;
+        next();
+    } catch (err) {
         return res.status(401).json({
-          message: "Please Login...!",
-          error: true,
-          success: false,
-        });
-      }
-  
-      jwt.verify(token, process.env.TOKEN_SECRET_KEY, (err, decoded) => {
-        if (err) {
-          return res.status(401).json({
             message: "Invalid token",
             error: true,
             success: false,
-          });
-        }
-  
-        req.userId = decoded?._id;
-        next();
-      });
+        });
+    }
+
+    req.userId = decoded?._id;
+    next();
+});
     } catch (err) {
-      res.status(400).json({
+    res.status(400).json({
         message: err.message || err,
         data: [],
         error: true,
         success: false,
-      });
-    }
+    });
+}
   }
 
 
