19bf22574f054e25aacbe03be5ed07b8c4403fcf
Your Name <your-email@example.com>
2024-08-24 16:50:44 +0530

    changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const AwlModel = require('./models/AwlModel');
 const generateToken = require('./utils/TokenUtils')
 
 const app = express()
-const allowedOrigins = ['https://66bc56186652184c2bb2efba--teal-yeot-b589c2.netlify.app','https://teal-yeot-b589c2.netlify.app','http://localhost:3001','http://localhost:3000','https://heroic-malasada-7f5dba.netlify.app','https://glowing-crisp-4c314b.netlify.app'];
+const allowedOrigins = ['https://66bc56186652184c2bb2efba--teal-yeot-b589c2.netlify.app','https://teal-yeot-b589c2.netlify.app','http://localhost:3001','http://localhost:3000','https://heroic-malasada-7f5dba.netlify.app','https://glittering-gecko-70b097.netlify.app'];
 app.use(cors({
     origin: function (origin, callback) {
         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
